3cff94e7195bf4660438a833ac60bc915f59db65
William Escobar <WilliamxEscobar@gmail.com>
2020-05-03 22:00:10 -0400

    Worked on logining in.

--- main.py
+++ main.py
@@ -15,6 +15,24 @@ class IGBot:
     def login(self):
         driver = self.driver
         driver.get("https://www.instagram.com/")
+        time.sleep(3)
+
+        loginButton = driver.find_element_by_xpath("//a[@href'accounts/login]")
+        loginButton.click()
+        time.sleep(2)
+
+        user_name_element = driver.find_element_by_xpath("//input[@name='username']")
+        user_name_element.clear()
+        user_name_element.send_keys(self.username)
+        time.sleep(2)
+
+        password_element = driver.find_element_by_xpath("//input[@name='password']")
+        password_element.clear()
+        password_element.send_keys(self.password)
+        time.sleep(2)
+
+        password_element.send_keys(Keys.RETURN)
 
 
-IGBot.login(h)
+myBot = IGBot("thewillescobar", "PromiseE@12")
+myBot.login()
