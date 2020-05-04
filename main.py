8a6a2d490658e447c4290fc52ce56efb4fb0bcc7
William Escobar <WilliamxEscobar@gmail.com>
2020-05-03 21:22:03 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class IGBot:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome()
+
+    def closeBrowser(self):
+        self.driver.close()
+
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com/")
+
+
+IGBot.login(h)
